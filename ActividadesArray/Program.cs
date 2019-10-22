a252672f0d49d3046b202107ca55300487dd1d8a
xavieroldan <xavieroldan@gmail.com>
2019-10-22 13:41:51 +0200

    Actividades Array

--- ActividadesArray/Program.cs
+++ ActividadesArray/Program.cs
@@ -10,16 +10,59 @@ namespace ActividadesArray
     {
         static void Main(string[] args)
         {
+            //Main menu
+            
+        mainMenu:;
+            Console.WriteLine("Elige el ejercicio que quieres hacer pulsando el número:");
+            Console.WriteLine("1-Múltiplos");
+            Console.WriteLine("2-Alumnos trimestre");
+            Console.WriteLine("3-Subida de suledo");
+            Console.WriteLine("4-Salir");
+            try
+            {
+                var option = Console.ReadKey();
+                switch (option.Key)
+                {
+                    case ConsoleKey.D1:
+                        goto sub1;
+                    case ConsoleKey.D2:
+                        goto sub2;
+                    case ConsoleKey.D3:
+                        goto sub3;
+                    case ConsoleKey.D4:
+                        goto end;
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                goto mainMenu;
+            }
+            Console.WriteLine("Pulsa una tecla para continuar.");
+            Console.ReadKey();
+            goto mainMenu;
 
-            // 1 - Ens entren n números , es necessiten desar i dir quants són múltiples de 5 i quants són
-            // múltiples de 7. (Tenir en compte que un número pot ser tant múltiple de cinc com de
-            // 7)
+        // 1 - Ens entren n números , es necessiten desar i dir quants són múltiples de 5 i quants són
+        // múltiples de 7. (Tenir en compte que un número pot ser tant múltiple de cinc com de
+        // 7)
+
+        sub1:;
             int cant = 0, num, mult5 = 0, mult7 = 0, multBoth = 0;
             int[] numList = new int[0];
-
+            
             Console.WriteLine("Múltiplos de 5 y 7\n");
+
+            repeat1:;
             Console.WriteLine("Indica el número de entradas:");
-            cant = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                cant = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                goto repeat1;
+            }
 
             Array.Resize(ref numList, cant);
 
@@ -40,10 +83,10 @@ namespace ActividadesArray
             }
             Console.WriteLine("Múltiplos de 5: {0} \nMúltiplos de 7: {1} \nMúltiplos de ambos {2}", mult5, mult7, multBoth);
 
-            //2 - Tenim un array amb la llista del 10 alumnes que hi ha a un curs.S’ha de desar les notes
-            //de cada trimestre(són tres trimestres ) per a cada alumne. Donar la mitja final dels
-            //trimestres del darrer alumne. 
-
+        //2 - Tenim un array amb la llista del 10 alumnes que hi ha a un curs.S’ha de desar les notes
+        //de cada trimestre(són tres trimestres ) per a cada alumne. Donar la mitja final dels
+        //trimestres del darrer alumne. 
+        sub2:;
             string[] students = { "Pepe", "Ana", "Litus", "Jose", "Gabi", "Sonia", "Lourdes", "Alex", "Mathew", "Eric" };
             double[] trim1 = new double[10];
             double[] trim2 = new double[10];
@@ -67,10 +110,13 @@ namespace ActividadesArray
 
             Console.WriteLine("El último alumno, {0}, tiene una media de: {1}", students[students.Length - 1], avg);
 
-            //                3 - Es tenen desats de manera interna els identificadors dels treballadors i a la vegada el
-            //seu sou i els anys que porten treballant. Als que porten més de 3 anys tenen un
-            //augment del 5 %.Actualitzar les dades del sou amb aquest augment.
-
+            Console.WriteLine("Pulsa una tecla para continuar.");
+            Console.ReadKey();
+            goto mainMenu;
+        //3 - Es tenen desats de manera interna els identificadors dels treballadors i a la vegada el
+        //seu sou i els anys que porten treballant. Als que porten més de 3 anys tenen un
+        //augment del 5 %.Actualitzar les dades del sou amb aquest augment.
+        sub3:;
             string[] listEmployee = { "id1", "id2", "id3" };
             double[] salaries = { 1000, 2000, 3000 };
             int[] seniority = { 10, 1, 4 };
@@ -88,9 +134,14 @@ namespace ActividadesArray
                 Console.WriteLine("Empleado: {0} \tAntigüedad:{1} años\tSueldo: {2}",listEmployee[i],seniority[i],salaries[i]);
             }
 
-            //End
+            Console.WriteLine("Pulsa una tecla para continuar.");
             Console.ReadKey();
-            //
+            goto mainMenu;
+
+
+        end:;
+            Console.WriteLine("\nAdios!!!");
+            Console.ReadKey();
         }
     }
 }

92761dbc3b6e8879b880a8b9b33589fb87db504c
xavieroldan <xavieroldan@gmail.com>
2019-10-22 16:10:54 +0200

    22/10/2019

---
+++ ArraysMultidimensional/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysMultidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a2 = new int[3, 4]; //2 dimensions
+            int[,,] b3 = new int[4, 8, 9]; //3 dimensions
+            int[][] c4 = new int[3][]; //not defined dimension
+
+            Console.Title = "Hello Mundo";
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            //Matrices "escalonadas"
+            int[][] jaggedArray =
+                {
+                    new int[] { 1, 3, 5, 7, 9 },
+                    new int[] { 0, 2, 4, 6 },
+                    new int[] { 11, 22 }
+                };
+
+            //Read array
+            int[,] myArray = new int [3, 4] { { 1, 2, 3, 4 } , { 5, 6, 7, 8 } , { 9, 10, 11, 12 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //Console.WriteLine(myArray[i, j]); 
+                }
+            }
+
+            //Exercise 1
+            int x, y;
+            var rand = new Random();
+            int colSize = 4;
+            int rowSize = 2;
+            int marginX = 25;
+            int marginY = 5;
+
+            Console.WriteLine("Dame filas:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame columnas:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int[,] customArray= new int[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    customArray[i, j] = rand.Next(0,99);
+                    Console.SetCursorPosition((colSize * j + marginX), (rowSize * i + marginY));
+                    Console.Write(customArray[i, j]);
+                }
+            }
+
+            
+            //End
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Colecciones/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList
+
+            //ArrayList myArrayList = new ArrayList();           
+
+            //myArrayList.Add(1);
+            //myArrayList.Add("texto");
+            //myArrayList.Add(true);
+            //myArrayList.Add(Console.BackgroundColor);
+
+            //IList myIlist = new ArrayList() //Interface where ArrayList behavior from
+            //{
+            //    100,200,true,"text"
+            //};
+
+            //myArrayList.AddRange(myIlist);
+
+            //int myVar1 = Convert.ToInt32(myArrayList[0]);
+            //string myVar2 = Convert.ToString(myArrayList[1]);
+            //bool myVar3 = Convert.ToBoolean(myArrayList[2]);
+            //var myVar4 = myArrayList[3];
+
+            //foreach (var item in myArrayList)
+            //{
+            //    Console.WriteLine("Objeto: "+item);
+            //}
+
+            //Entrada de alumnos y notas en un ArrayList
+            ArrayList classList = new ArrayList();
+            double noteAvg = 0.0d;
+            int count = 1, option;
+
+            Console.WriteLine("Número de alumnos:");
+            int size= Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Nombre alumno:");
+                classList.Add(Console.ReadLine());
+                Console.WriteLine("Nota:");
+                classList.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+            Console.WriteLine("Elige un alumno para saber su nota:");
+            foreach (var item in classList)
+            {
+                
+                if(item is Double)
+                { 
+                    noteAvg += Convert.ToDouble(item);
+                }
+                else
+                {
+                    Console.WriteLine(count+"-"+item);
+                    count++;
+                }
+            }
+            noteAvg /= size;
+            Console.WriteLine("La media de notas es de: {0:F2}",noteAvg);
+            option = Convert.ToInt16(Console.ReadLine());
+
+            count = 0;
+            foreach (var item in classList)
+            {
+                if(item is String)
+                {                    
+                    count++;
+                }
+                else if(count == option)
+                {
+                    Console.WriteLine("Nota:"+item);
+                }
+            }
+
+            //End
+            Console.ReadKey();
+        }
+    }
+}
